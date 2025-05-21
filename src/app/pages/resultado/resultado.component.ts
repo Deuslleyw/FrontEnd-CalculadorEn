4d359863c7b7924fc2669996f0da1ae739366a2a
Deuslleyw <Deuslleyw@hotmail.com>
2025-05-21 20:36:05 -0300

    impl/result

--- src/app/pages/resultado/resultado.component.ts
+++ src/app/pages/resultado/resultado.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-resultado',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './resultado.component.html',
   styleUrl: './resultado.component.scss'
 })
 export class ResultadoComponent {
+  resultado: string | null = '0';
+
+  ngOnInit(){
+    this.resultado = localStorage.getItem('resultado');
+  }
 
 }
